284a147745694fa45bbb321a081cb9022eb6b432
babua <onurbabacan@gmail.com>
2016-06-08 18:55:29 +0300

    debugging

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ tg.inlineMode(($) => {
 		    			type: "photo",
 		    			id : val.slice(0,36),
 		    			photo_url : config.serverUrl + $.from.id + "/" + val,
-		    			thumb_url : config.serverUrl + $.from.id + "/thumbnails/" + val.slice(0,val.lastIndexOf(".")-1) + "-100x100" + path.extname(val)
+		    			thumb_url : config.serverUrl + $.from.id + "/thumbnails/" + val.slice(0,val.lastIndexOf(".")) + "-100x100" + path.extname(val)
 		    		});
 		    	});
 		    	console.log(results);
