a6e8e36799a69c6f9c6b14d561272b8e0f5d1afe
babua <onurbabacan@gmail.com>
2016-06-08 18:42:27 +0300

    debugging

--- index.js
+++ index.js
@@ -112,8 +112,8 @@ tg.inlineMode(($) => {
 		    			thumb_url : config.serverUrl + $.from.id + "/thumbnails/" + val.slice(0,val.lastIndexOf(".")-1) + "-100x100" + path.extname(val)
 		    		});
 		    	});
-		    	console.log(results)
-		    	$.paginatedAnswer(results, 10);
+		    	console.log(results);
+		    	$.paginatedAnswer(results, 20);
 
 		    });
     	}
