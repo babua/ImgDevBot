43a8e6959b8ff81ff50cc2a6a85a8d8c7d34a4bb
babua <onurbabacan@gmail.com>
2016-06-08 18:11:52 +0300

    first commit

---
+++ config/config.sample.js
@@ -0,0 +1,8 @@
+module.exports = {
+        "fileSizeLimit" : 5000000,
+        "fileSizeLimitHumanReadable" : "5Mb",
+        "imageFolder" : "./images/",
+        telegram : {
+                "token" : ''
+        }
+};
